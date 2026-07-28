bcfd957cadbdc2457d63ec43d98555d2238c8af8
alvarocontreras-spec <alvaro.contreras@colegiosanignacio.cl>
2026-07-28 15:32:18 -0400

    Enhance song data with adjectives and superlatives
    Added adjectives and superlatives to song objects.

--- songs.js
+++ songs.js
@@ -1,17 +1,73 @@
+/* ==========================================
+   SONG DATABASE
+   Music Awards Homework
+========================================== */
+
 const songs = [
 
 {
+    id:1,
+
     artist:"Taylor Swift",
+
     title:"Shake It Off",
+
     youtube:"https://www.youtube.com/embed/nfWlot6h_JM",
-    image:"assets/taylor.jpg"
+
+    adjective:"more popular",
+
+    superlative:"the most popular"
 },
 
 {
+    id:2,
+
     artist:"Bruno Mars",
+
     title:"Count on Me",
+
     youtube:"https://www.youtube.com/embed/MpLHIvCQEek",
-    image:"assets/bruno.jpg"
+
+    adjective:"more talented",
+
+    superlative:"the most talented"
 }
+
+];
+
+
+/* ==========================================
+WORD BANK
+========================================== */
+
+const wordBank = [
+
+"better",
+"best",
+
+"more popular",
+"the most popular",
+
+"more talented",
+"the most talented",
+
+"more exciting",
+"the most exciting",
+
+"more beautiful",
+"the most beautiful",
+
+"more interesting",
+"the most interesting",
+
+"also",
+"moreover",
+"in addition",
+
+"because",
+
+"I think",
+
+"In my opinion"
 
 ];
