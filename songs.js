5cc5f032ab289030387c5cafa2a8800cd52e88e5
alvarocontreras-spec <alvaro.contreras@colegiosanignacio.cl>
2026-07-28 13:20:06 -0400

    Add songs.js with song details and attributes

---
+++ songs.js
@@ -0,0 +1,22 @@
+const songs = [
+{
+artist:"Taylor Swift",
+song:"Shake It Off",
+video:"https://www.youtube.com/embed/nfWlot6h_JM",
+adjectives:[
+"popular",
+"talented",
+"famous"
+]
+},
+{
+artist:"Bruno Mars",
+song:"Count on Me",
+video:"https://www.youtube.com/embed/MpLHIvCQEek",
+adjectives:[
+"friendly",
+"good",
+"relaxing"
+]
+}
+];
